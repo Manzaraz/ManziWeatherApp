4ea38040c7976eacce7b38c4a4154f33134fff6d
Christian Manzaraz <magnusmanz@gmail.com>
2024-08-01 16:47:17 -0300

    Initial Commit

---
+++ ManziWeatherApp/ManziWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManziWeatherAppApp.swift
+//  ManziWeatherApp
+//
+//  Created by Christian Manzaraz on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ManziWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
